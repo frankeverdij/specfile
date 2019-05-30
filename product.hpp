6470407212d53e76d9e7c76f5e719c2dcb63eec7
frankeverdij <F.P.X.Everdij@tudelft.nl>
2019-05-30 11:19:11 +0200

    added base/derived class separation for product class

--- product.hpp
+++ product.hpp
@@ -1,20 +1,59 @@
 #include <string>
 #include <vector>
-#include "image.hpp"
 
 #pragma once
 
-class product
+class item
+{
+    protected:
+        string name_;
+        string id_;
+        size_t offset_;
+    
+    public:
+        void setName(string &);
+        string getName();
+        void setDescription(string &);
+        string getDescription();
+        void setOffset(size_t &);
+        size_t getOffset();
+}
+
+class subsystem : public item
+{
+    private:
+        string expanded_name_;
+        std::vector<subref> replaces_;
+        std::vector<subref> prereq_;
+        std::vector<subref> incompat_;
+
+    public:
+        subsystem();
+
+}
+
+class image : public item
+{
+    private:
+        unsigned int version_;
+        size_t order_;
+        std::vector<subsystem> subsystems;
+
+    public:
+        image();
+
+}
+
+class product : public item
 {
     private:
         time_t creation_time_;
-        string name_;
-        string description_;
-        size_t offset_;
         std::vector<image> images;
 
     public:
         product();
+        void setTime(time_t &);
         time_t getTime();
         
 };
+
